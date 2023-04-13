db80cbb3599f65d3fbeeca1ab80d09bb81fbb63a
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-13 22:10:57 +0200

    add methods

--- js/script.js
+++ js/script.js
@@ -10,8 +10,24 @@ createApp({
         'img/03.webp',
         'img/04.webp',
         'img/05.webp' 
-      ]
+      ],
+      counter:0,
+    
     }
+  },
+
+  methods:{
+    nextPrev(){
+      if (prev)this.counter++;
+      else this.counter--;
+  
+      if(this.counter > this.images.lenght) this. counter = 0;
+      if(this.counter < 0) this.counter = this.images.lenght -1;
+
+    }
+    
   }
 
 }).mount('#app')
+
+// isNext
